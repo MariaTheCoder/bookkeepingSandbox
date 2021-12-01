06a5898158158ccaff62a0eea445b49eed52df21
Maria T. Andersen <mteilmann93@gmail.com>
2021-12-01 23:06:16 +0100

    upon save, add new data to existing data and display text of newly added post in the document. Later we will create elements to display date, amount, currency, account and offset account as well

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 let inputData = {};
+let storedData = [];
 
 document.getElementById("save-button").addEventListener("click", function () {
-  inputData.date = document.getElementById("date").value;
-  inputData.text = document.getElementById("text").value;
-  inputData.amount = document.getElementById("amount").value;
-  inputData.currency = document.getElementById("currency").value;
-  inputData.account = document.getElementById("account").value;
-  inputData.offsetAccount = document.getElementById("offset-account").value;
-  console.log(inputData);
+  // firstly, we want to save and store the input data in the inputData object
+  inputData = {
+    date: document.getElementById("date").value,
+    text: document.getElementById("text").value,
+    amount: document.getElementById("amount").value,
+    currency: document.getElementById("currency").value,
+    account: document.getElementById("account").value,
+    offsetAccount: document.getElementById("offset-account").value,
+  };
+
+  // print with console.log to ensure that the data looks right
+  storedData.unshift(inputData);
+  // console.log("inputData", inputData);
+  console.log("storedData", storedData);
+
+  // create a new post in the bookkeeping containing the saved information
+  // use insertBefore to place the most recently created post on top of existing posts
+  let newPost = document.createElement("p");
+  newPost.innerText = storedData[0].text;
+  document
+    .getElementById("date")
+    .parentElement.parentElement.children[1].insertBefore(
+      newPost,
+      document.getElementById("date").parentElement.parentElement.children[1]
+        .children[0]
+    );
 });
