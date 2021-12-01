882b406401a20532863363c21e2cafc203dc1c12
Maria T. Andersen <mteilmann93@gmail.com>
2021-12-01 20:45:28 +0100

    create input fields for various information and upon click on save button, store the input data in an object and display in browser

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+let inputData = {};
+
+document.getElementById("save-button").addEventListener("click", function () {
+  inputData.date = document.getElementById("date").value;
+  inputData.text = document.getElementById("text").value;
+  inputData.amount = document.getElementById("amount").value;
+  inputData.account = document.getElementById("account").value;
+  inputData.offsetAccount = document.getElementById("offset-account").value;
+  console.log(inputData);
+});
