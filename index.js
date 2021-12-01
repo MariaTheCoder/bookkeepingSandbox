d0bbfa909639c4c367070d7b52b24560a8546ccc
Maria T. Andersen <mteilmann93@gmail.com>
2021-12-01 20:57:38 +0100

    add a dropdown for the option to add a currency and store the chosen value upon click on save button along with the other data that is already saved in the inputData object

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ document.getElementById("save-button").addEventListener("click", function () {
   inputData.date = document.getElementById("date").value;
   inputData.text = document.getElementById("text").value;
   inputData.amount = document.getElementById("amount").value;
+  inputData.currency = document.getElementById("currency").value;
   inputData.account = document.getElementById("account").value;
   inputData.offsetAccount = document.getElementById("offset-account").value;
   console.log(inputData);
