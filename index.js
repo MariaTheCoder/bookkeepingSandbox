f4a72afc9a2512c11b1d7194855618f8741b1763
Maria T. Andersen <mteilmann93@gmail.com>
2021-12-02 09:18:35 +0100

    impliment a function to create new div elements and append to parent element

--- index.js
+++ index.js
@@ -14,18 +14,66 @@ document.getElementById("save-button").addEventListener("click", function () {
 
   // print with console.log to ensure that the data looks right
   storedData.unshift(inputData);
-  // console.log("inputData", inputData);
-  console.log("storedData", storedData);
 
   // create a new post in the bookkeeping containing the saved information
+  let newPost = document.createElement("div");
+
+  // use function down below to create new elements, to set inner text and append children to parent element
+  const newDate = createAndAppendDivChild("div", storedData[0].date, newPost);
+  const newText = createAndAppendDivChild("div", storedData[0].text, newPost);
+  const newAmount = createAndAppendDivChild(
+    "div",
+    storedData[0].amount,
+    newPost
+  );
+  const newCurrency = createAndAppendDivChild(
+    "div",
+    storedData[0].currency,
+    newPost
+  );
+  const newAccount = createAndAppendDivChild(
+    "div",
+    storedData[0].account,
+    newPost
+  );
+  const newOffsetAccount = createAndAppendDivChild(
+    "div",
+    storedData[0].offsetAccount,
+    newPost
+  );
+
+  firstChildOfParentElement =
+    document.getElementById("text").parentElement.parentElement.children[1];
+  console.log(firstChildOfParentElement);
+
   // use insertBefore to place the most recently created post on top of existing posts
-  let newPost = document.createElement("p");
-  newPost.innerText = storedData[0].text;
-  document
-    .getElementById("date")
-    .parentElement.parentElement.children[1].insertBefore(
-      newPost,
-      document.getElementById("date").parentElement.parentElement.children[1]
-        .children[0]
-    );
+  // document
+  //   .getElementById("date")
+  //   .parentElement.parentElement.children[1].insertBefore(
+  //     newDate,
+  //     document.getElementById("date").parentElement.parentElement.children[1]
+  //       .children[0]
+  //   );
+  // document
+  //   .getElementById("text")
+  //   .parentElement.parentElement.children[1].insertBefore(
+  //     newText,
+  //     document.getElementById("text").parentElement.parentElement.children[1]
+  //       .children[0]
+  //   );
+
+  // document
+  //   .getElementById("post-container")
+  //   .insertBefore(
+  //     newPost.children[0].innerText,
+  //     document.getElementById("text").parentElement.parentElement.children[1]
+  //       .children[0]
+  //   );
 });
+
+function createAndAppendDivChild(tag, innerText, parent) {
+  let element = document.createElement(tag);
+  element.innerText = innerText;
+  parent.appendChild(element);
+  return element;
+}
