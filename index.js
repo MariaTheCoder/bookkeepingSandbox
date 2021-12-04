8fefabbe86817906e6ca52e6580b8fc83fdef602
Maria T. Andersen <mteilmann93@gmail.com>
2021-12-04 06:01:03 +0100

    ensure that document numbers are compared to stored data as numbers, not strings

--- index.js
+++ index.js
@@ -136,7 +136,8 @@ function repeatedDocNumber(docNumberInQuestion) {
   for (let i = 0; i < storedData.length; i++) {
     const post = storedData[i];
 
-    if (post.documentNumber === docNumberInQuestion) return true;
+    if (Number(post.documentNumber) === Number(docNumberInQuestion))
+      return true;
   }
   return false;
 }
