375b5cbe22bbb1e8fd4650db64931722888a9531
Maria T. Andersen <mteilmann93@gmail.com>
2022-03-06 09:52:19 +0100

    add click event to the documentNumber header

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const inputAccount = document.getElementById("account");
 const createNewPostButton = document.getElementById("create-new-post");
 const deleteAllPostsButton = document.getElementById("delete-all-posts");
 
+// headers
+const documentNumberHeader = document.getElementById("document_number");
+
 if (inputAccount)
   inputAccount.addEventListener("keyup", () => {
     isCreditDefault(Number(inputAccount.value))
@@ -16,3 +19,8 @@ if (createNewPostButton)
   createNewPostButton.addEventListener("click", validateInput);
 if (deleteAllPostsButton)
   deleteAllPostsButton.addEventListener("click", clearDB);
+
+if (documentNumberHeader)
+  documentNumberHeader.addEventListener("click", () =>
+    console.log("I was clicked!")
+  );
