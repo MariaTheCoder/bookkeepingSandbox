f09cd84f7a8ab64b5a8e85ced67c4dc0e37d1b05
Maria T. Andersen <mteilmann93@gmail.com>
2022-03-10 17:49:15 +0100

    edit this function to clear up the container for post headers, post details and the delete button upon being called at the beginning of render function

--- deleteAllPosts.js
+++ deleteAllPosts.js
@@ -1,3 +1,5 @@
 function deleteAllPosts() {
+  document.getElementById("post-header-container").innerHTML = "";
   document.getElementById("created-posts").innerHTML = "";
+  document.getElementById("delete-button-container").innerHTML = "";
 }
