0917b433727ccdd03f599ff630acaeaaa10e92e1
Maria T. Andersen <mteilmann93@gmail.com>
2022-03-10 19:20:17 +0100

    delete console.log command

--- removeDataObjectFromDB.js
+++ removeDataObjectFromDB.js
@@ -1,5 +1,4 @@
 function removeDataObjectFromDB(documentNumber) {
-  console.log(`clicked element: ${documentNumber}`);
   for (let i = 0; i < storedData.length; i++) {
     const documentNumberOfStoredData = storedData[i].columns["documentNumber"];
 
