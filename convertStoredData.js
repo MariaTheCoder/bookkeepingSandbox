dd2eba7b46fc73902091562c17166a6d273e7789
Maria T. Andersen <mteilmann93@gmail.com>
2022-03-26 22:06:59 +0100

    refactoring

--- convertStoredData.js
+++ convertStoredData.js
@@ -1,28 +1,28 @@
 function convertStoredData() {
-  let myArray = [];
-  let singleHeaders = [];
+  const csvRows = [];
+  const singleHeaders = [];
 
   for (let i = 0; i < storedData.length; i++) {
-    const object = storedData[i].columns;
     // variable to store values of one row, or all values of one column object inside of StoredData in form of an array
-    let row = [];
+    const row = [];
+    const columns = storedData[i].columns;
 
-    for (const key in object) {
+    for (const column in columns) {
       // firstly, get the headers just once
       if (i === 0) {
-        singleHeaders.push(key);
+        singleHeaders.push(column);
       }
 
       // store values of each property inside of the StoredData[i].columns objects inside of innerArray
       // push each value to the array 'row'
-      const value = object[key];
+      const value = columns[column];
       row.push(value);
     }
-    myArray.push(row);
+    csvRows.push(row);
   }
 
   //convert headers into a format that can be directly used for our csv file
   const headers = `${singleHeaders.join(";")}\n`;
 
-  return [headers, myArray];
+  return [headers, csvRows];
 }
