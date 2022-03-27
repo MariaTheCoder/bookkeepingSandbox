3443192bf08d8b44efbcfdeea43ace424c9f6761
Maria T. Andersen <mteilmann93@gmail.com>
2022-03-27 09:59:04 +0200

    enable empty values for fields in the offsetAccount column

--- isCorrectInputType.js
+++ isCorrectInputType.js
@@ -17,10 +17,12 @@ function isCorrectInputType(inputColumnData) {
       (expectedTypes[attribute] === "string" &&
         expectedTypes[attribute] !== typeof element.value) ||
       (expectedTypes[attribute] === "number" &&
-        isNaN(parseFloat(element.value))) ||
+        isNaN(parseFloat(element.value)) &&
+        attribute === "amount") ||
       (expectedTypes[attribute] === "number" &&
-        !!element.value &&
-        element.value <= 0)
+        attribute !== "amount" &&
+        !!parseFloat(element.value) &&
+        parseFloat(element.value) <= 0)
     )
       validatorObject[attribute] = false;
   }
